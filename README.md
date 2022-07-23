7d1f2807897a039ab54cd5e51fc211988a3f6eb5
fabrizzio <fabrizziorp7@gmail.com>
2022-07-23 15:44:00 -0400

    first commit :p

---
+++ README.md
@@ -0,0 +1,10 @@
+# SOFT MULTI BASH
+
+1. first step.
+```
+chmod +x save-x.sh
+```
+2. second step.
+```
+./save-x.sh
+```
---
+++ save-x.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+
+echo -n "Write any number : "
+read val1
+
+for(( i=1; i<=$val1; i++  )) do
+	clear
+	echo "============"
+	echo "Table of $i"
+	echo "============"
+	for (( j=1; j<=12; j++  )) do
+		result=$((i*j))
+		echo "$i x $j = "$result
+	done
+done >> results.txt
+
+echo "Your file is created correctly on this path : "
+echo $(pwd)/results.txt
